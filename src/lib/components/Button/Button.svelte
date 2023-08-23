9573caca313c4be607b32f2375947bd15f100e3e
Kris Kuiper <kris@mooore.nl>
2023-08-23 16:08:47 +0200

    feat: add page for components

---
+++ src/lib/components/Button/Button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let suffix = "";
+</script>
+
+<button on:click>
+  <slot />
+
+  {#if suffix}
+    {suffix}
+  {/if}
+</button>
+
+<style>
+  button {
+    padding: 1em 2em;
+    border: 0;
+    border-radius: 0.75em;
+    background: pink;
+    font-weight: 700;
+    display: block;
+  }
+
+  button:active {
+    transform: scale(0.98);
+  }
+</style>
---
+++ src/routes/components/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Button from '$lib/components/Button';
+</script>
+
+<div>
+  <h1>1. Componenten</h1>
+  <p>Dit is een component:</p>
+
+  <Button>Knopje!</Button>
+
+  <a href="/components/props">
+    Cool, what about props?
+  </a>
+</div>
